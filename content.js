4f7e7b418fe88bb52b8a7426e6db1aae35e797af
tekkub <tekkub@gmail.com>
2010-01-10 23:59:22 -0700

    Cleanup

--- content.js
+++ content.js
@@ -1,51 +1,27 @@
 
 $(function() {
-  var name_map = {
-    "David Shafer" : "tekkub",
-    "Tom P Finn" : "Dolby",
-    "Pete Lawler" : "Bleeter",
-    "Jim Whitehead" : "Cladhaire",
-    "Jenn Crewell" : "Lopeppeppy",
-    "George Pribul" : "Kirkburn",
-    "Ken Allan" : "Norganna",
-    "Sin Huan Chan" : "Xinhuan",
-    "Esteban Santana Santana" : "Mentalpower",
-    "Ryan Lakanen" : "Seerah",
-    "Mark Breitenbach" : "zhinjio",
-    "Jacob Segal" : "Guillotine",
-    "Jim Garner" : "Kudane",
-    "Matthew Orlando" : "cogwheel",
-    "Bernie Fritts" : "Bouvi",
-    "Taner Halicioglu" : "Duman",
-    "Matt Converse" : "Lunessa",
-    "Karen Cairenn Hobson" : "Cairenn",
-    "Kevin M. Kelly" : "Zootfizzle",
-    "Kelly Crabbé" : "Moonwitch",
-    "John Pasula" : "Ackis",
-    "Bryan McLemore" : "Kaelten",
-    "Danial Bakker" : "Imios",
-    "James Callahan" : "Torhal",
-    "Mikael Olsson" : "Mikk",
-  }
-
   var inrename = false;
   var rename_users = function(links) {
     if (!inrename) {
       inrename = true;
       links.each(function(i) {
         var realname = $(this).text();
-        var alias = name_map[realname];
+        var alias = localStorage[realname];
         if (alias) {$(this).text(alias);}
+        if ($(this).hasClass("GenericStory_Name") && !$(this).hasClass("has_a_renamer")) {
+          $(this).addClass("has_a_renamer").after(
+            $("<a>").text("✍").css("margin", "0em 0.25em").click(function () {
+              var newname = prompt("Enter new name for " + realname + ".  Delete name to reset.  Changes will appear on next page reload.", alias || realname);
+              if (newname != "null") {localStorage[realname] = newname || realname;}
+              return false;
+            })
+          )
+        }
       })
       inrename = false;
     }
   }
 
-  $("body").bind("DOMNodeInserted", function(){
-    rename_users($(this).find("a"))
-  });
+  $("body").bind("DOMNodeInserted", function(){rename_users($(this).find("a"))});
   rename_users($("a"));
-  // rename_users();
 });
-
-
